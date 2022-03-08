5860c84a2c8ed6dcaccce1b6a15199a974ba1014
NoobTenLuka <42914240+NoobTenLuka@users.noreply.github.com>
2022-03-08 16:10:34 +0100

    Fügt Beispiel hinzu

---
+++ beispiele/beispiel-01.c
@@ -0,0 +1,22 @@
+#include "../deutsch.h"
+#include <stdio.h>
+
+ganzzahl hauptfunktion oeffnen
+    wenn wahr dann
+        druckenf("Hallo Welt!\n") semikolon
+    ansonsten wenn falsch dann 
+        druckenf("Das wird nie ausgeführt.\n") semikolon
+    schliessen
+    
+    ganzzahl i ist 0;
+    waehrend i kleinergleich 5 dann
+        druckenf("Hallo Nr. %d \n", i) semikolon
+        i plusgleich 2 semikolon
+    schliessen
+    
+    fuer ganzzahl j ist 3 semikolon j groesser 0 semikolon j minusminus mache
+        druckenf("Hallo aus der Für-Schleife. Countdown: %d \n", j) semikolon
+    schliessen
+    
+    zurueckgeben 0 semikolon
+schliessen
