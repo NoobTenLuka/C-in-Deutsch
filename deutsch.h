e887b9e325ea5d1797e4a065ab3db8ae66bcd8c6
NoobTenLuka <42914240+NoobTenLuka@users.noreply.github.com>
2022-03-08 16:06:06 +0100

    Create deutsch.h

---
+++ deutsch.h
@@ -0,0 +1,51 @@
+#include <stdbool.h>
+
+#define hauptfunktion main()
+
+#define ganzzahl int
+#define zeichen char
+#define kurz short
+#define doppel double
+
+#define oeffnen {
+#define schliessen }
+
+#define ist =
+#define gleich ==
+#define kleiner <
+#define kleinergleich <=
+#define groesser >
+#define groessergleich >=
+
+#define plus +
+#define plusgleich +=
+#define minus -
+#define minusgleich -=
+#define mal *
+#define malgleich *=
+#define geteilt /
+#define geteiltgleich /=
+#define modulo %
+#define modulogleich %=
+
+#define plusplus ++
+#define minusminus --
+
+#define wenn if (
+#define dann ) {
+#define ansonsten } else
+
+#define waehrend while (
+    
+#define fuer for (
+#define mache ) {
+    
+#define drucken print
+#define druckenf printf
+
+#define wahr true
+#define falsch false
+
+#define semikolon ;
+#define zurueckgeben return
+    
